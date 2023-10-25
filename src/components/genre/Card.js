a88ac60e530b50eccbe4bb04e9c860cc4058ae58
Swati <swatic019@gmail.com>
2023-10-25 22:47:18 +0530

    activeDiv class implemented

--- src/components/genre/Card.js
+++ src/components/genre/Card.js
@@ -1,26 +1,26 @@
 import "./category.css";
 
 export default function Card({ data, categories, setCategories }) {
-  
   function handleCategoryClick() {
-    let duplicateCategories = categories.filter((item) => item.name === data.name);
+    let duplicateCategories = categories.filter(
+      (item) => item.name === data.name
+    );
 
     if (duplicateCategories.length > 0) {
       setCategories(categories.filter((item) => item.name !== data.name));
     } else {
       setCategories([...categories, data]);
     }
- 
-    console.log(categories)
-    console.log(data)
   }
 
   return (
     <>
       <div
-        className={`card_wrapper`}
+        className={`card_wrapper ${
+          categories.some((item) => item.name === data.name) ? "activeDiv" : ""
+        }`}
         onClick={handleCategoryClick}
-        style={{ background: data.bgColor}}
+        style={{ background: data.bgColor }}
       >
         <p>{data.name}</p>
         <img src={data.bgImg} alt="action" className="cardImg"></img>
