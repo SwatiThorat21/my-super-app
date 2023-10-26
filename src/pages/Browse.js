43815d89b4196a66c067c69758043d2c6468259b
Swati <swatic019@gmail.com>
2023-10-26 12:24:13 +0530

    Browse page added

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,9 +1,22 @@
-
+import Profile from "../components/Browse/Profile";
+import Notes from "../components/Browse/Notes";
+import Weather from "../components/Browse/Weather";
+import Timer from "../components/Browse/Timer";
+import News from "../components/Browse/News";
 
 export default function Browse() {
   return (
     <>
-     <h1>This is Browse</h1>
+    <div className="browse_container">
+      <div className="left_grid_wrapper">
+        <Profile />
+        <Notes />
+        <Weather />
+        <Timer />
+      </div>
+      <News />
+      <button className="browseBtn">Browse</button>
+    </div>
     </>
   );
 }
